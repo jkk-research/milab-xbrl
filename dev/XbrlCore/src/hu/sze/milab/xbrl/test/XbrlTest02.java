3c20f0c2ffd03140ec94a05f730ff11665443cb9
lkedves <lkedves@localhost>
2023-07-14 21:12:50 +0200

    XBRL - follow Dust agent refactor, JSON reading

--- dev/XbrlCore/src/hu/sze/milab/xbrl/test/XbrlTest02.java
+++ dev/XbrlCore/src/hu/sze/milab/xbrl/test/XbrlTest02.java
@@ -13,14 +13,13 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 
 import hu.sze.milab.dust.Dust;
+import hu.sze.milab.dust.dev.DustDevConsts;
 import hu.sze.milab.dust.stream.DustStreamUrlCache;
-import hu.sze.milab.dust.stream.json.DustStreamJsonAgentParser;
-import hu.sze.milab.dust.stream.json.DustStreamJsonApiAgentSerializer;
 import hu.sze.milab.dust.stream.xml.DustStreamXmlAgentParser;
 import hu.sze.milab.dust.stream.xml.DustStreamXmlDocumentGraphLoader;
 import hu.sze.milab.xbrl.XbrlConsts;
 
-public class XbrlTest02 implements XbrlConsts {
+public class XbrlTest02 implements XbrlConsts, DustDevConsts {
 
 	private static File dataDir;
 	private static File out;
@@ -48,7 +47,8 @@ public class XbrlTest02 implements XbrlConsts {
 //				});
 		
 		readJsons(new String[] { 
-				"jsonapi_01",
+//				"jsonapi_01",
+				"banks",
 				});
 		
 		Dust.dumpObs("Process complete in", System.currentTimeMillis() - ts, "msec.");
@@ -96,12 +96,13 @@ public class XbrlTest02 implements XbrlConsts {
 	}
 
 	public static void readJsons(String[] args) throws Exception {
-		DustStreamJsonAgentParser aJson = new DustStreamJsonAgentParser();
-
-		aJson.agentExecAction(MindAction.Init);
-
-		MindHandle target = Dust.createHandle();
-		Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, target, MISC_ATT_CONN_TARGET);
+		
+//		DustStreamJsonAgentParser aJson = new DustStreamJsonAgentParser();
+//
+//		aJson.agentExecAction(MindAction.Init);
+//
+//		MindHandle target = Dust.createHandle();
+//		Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, target, MISC_ATT_CONN_TARGET);
 
 //		DustDevAgentDump dump = new DustDevAgentDump();
 //		dump.prefix = "Commit dump";
@@ -111,20 +112,36 @@ public class XbrlTest02 implements XbrlConsts {
 //		dump.ps = System.out;
 //		dump.ps = new PrintStream(new File(out, "jsonTest.json"));
 
-		DustStreamJsonApiAgentSerializer dump = new DustStreamJsonApiAgentSerializer();
+//		DustStreamJsonApiAgentSerializer dump = new DustStreamJsonApiAgentSerializer();
+//
+//		MindHandle listener = Dust.createHandle();
+//		Dust.access(listener, MindAccess.Set, dump, DUST_ATT_NATIVE_INSTANCE);
+//		Dust.access(target, MindAccess.Set, listener, MIND_ATT_KNOWLEDGE_LISTENERS);
+
+		Dust.dumpObs("Read JSON in XBRL");
+		
+		MindHandle hRead = Dust.createHandle();
+		Dust.access(hRead, MindAccess.Set, STREAM_LOG_JSONAPISERIALIZER, MIND_ATT_AGENT_LOGIC);
+		Dust.access(hRead, MindAccess.Set, hRead, MIND_ATT_KNOWLEDGE_LISTENERS);
+
+		MindHandle target = Dust.createHandle();
+		Dust.access(hRead, MindAccess.Set, target, MISC_ATT_CONN_TARGET);
 
 		MindHandle listener = Dust.createHandle();
-		Dust.access(listener, MindAccess.Set, dump, DUST_ATT_NATIVE_INSTANCE);
+		Dust.access(listener, MindAccess.Set, DEV_LOG_DUMP, MIND_ATT_AGENT_LOGIC);
 		Dust.access(target, MindAccess.Set, listener, MIND_ATT_KNOWLEDGE_LISTENERS);
 
 		for (String fn : args) {
 			File fIn = new File(in, fn + ".json");
+			
+			Dust.access(hRead, MindAccess.Set, fIn, STREAM_ATT_STREAM_FILE);
+			Dust.access(hRead, MindAccess.Commit, MindAction.Process);
 
-			Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, fIn, STREAM_ATT_STREAM_FILE);
-			aJson.agentExecAction(MindAction.Process);
+//			Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, fIn, STREAM_ATT_STREAM_FILE);
+//			aJson.agentExecAction(MindAction.Process);
 		}
 
-		aJson.agentExecAction(MindAction.Release);
+//		aJson.agentExecAction(MindAction.Release);
 
 //		dump.ps.flush();
 //		dump.ps.close();
@@ -138,7 +155,7 @@ public class XbrlTest02 implements XbrlConsts {
 		aXml.agentExecAction(MindAction.Init);
 
 		MindHandle target = Dust.createHandle();
-		Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, target, MISC_ATT_CONN_TARGET);
+//		Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, target, MISC_ATT_CONN_TARGET);
 
 //		DustDevAgentDump dump = new DustDevAgentDump();
 //		dump.prefix = "Commit dump";
@@ -163,7 +180,7 @@ public class XbrlTest02 implements XbrlConsts {
 
 			dump.init(target, outName);
 
-			Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, fIn, STREAM_ATT_STREAM_FILE);
+//			Dust.access(MIND_ATT_AGENT_SELF, MindAccess.Set, fIn, STREAM_ATT_STREAM_FILE);
 			aXml.agentExecAction(MindAction.Process);
 
 			File f = dump.save();
