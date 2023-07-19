419009f1be1b5fa80a84ac4c36130973a4374b81
lkedves <lkedves@localhost>
2023-07-19 16:23:09 +0200

    Parse banks.json with a custom reader
    Prepare for importing into the knowledge base.

---
+++ dev/XbrlCore/src/hu/sze/milab/xbrl/XbrlSourceAgentFilingsXbrlOrg.java
@@ -0,0 +1,10 @@
+package hu.sze.milab.xbrl;
+
+import hu.sze.milab.dust.dev.DustDevAgentDump;
+
+public class XbrlSourceAgentFilingsXbrlOrg extends DustDevAgentDump implements XbrlConsts {
+
+	public XbrlSourceAgentFilingsXbrlOrg() {
+		prefix = "FilingsXbrlOrg reader";
+	}
+}
