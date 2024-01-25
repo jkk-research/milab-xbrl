553296743e885cc4e6d2b5173da8698dfae53ba7
lkedves <lkedves@localhost>
2024-01-25 15:52:24 +0100

    XbrlTracer - EDGAR submissions unzip update works

--- dev/XbrlTracer/src/hu/sze/uni/xbrl/edgar/XbrlEdgarAgentUnzip.java
+++ dev/XbrlTracer/src/hu/sze/uni/xbrl/edgar/XbrlEdgarAgentUnzip.java
@@ -1,19 +1,55 @@
 package hu.sze.uni.xbrl.edgar;
 
+import java.io.File;
+
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipFile;
 
 import hu.sze.milab.dust.Dust;
 import hu.sze.milab.dust.DustAgent;
+import hu.sze.milab.dust.utils.DustUtils;
+import hu.sze.milab.dust.utils.DustUtilsApache;
+import hu.sze.milab.dust.utils.DustUtilsFile;
 
 public class XbrlEdgarAgentUnzip extends DustAgent implements XbrlEdgarConsts {
-	String path;
-	
+
+	@Override
+	public MindHandle agentBegin() throws Exception {
+		File fDataRoot = DustUtilsFile.getFile(MIND_TAG_CONTEXT_SELF, MISC_ATT_CONN_TARGET, RESOURCE_ATT_URL_PATH);
+		Dust.access(MIND_TAG_CONTEXT_SELF, MIND_TAG_ACCESS_SET, fDataRoot, MISC_ATT_CONN_TARGET, MISC_ATT_VARIANT_VALUE);
+		
+		return MIND_TAG_RESULT_READACCEPT;
+	}
+
 	@Override
 	public MindHandle agentProcess() throws Exception {
 		String name = Dust.access(MIND_TAG_CONTEXT_TARGET, MIND_TAG_ACCESS_PEEK, "", RESOURCE_ATT_URL_PATH);
-		
+
 		if ( name.endsWith(DUST_EXT_JSON) ) {
 			ZipArchiveEntry zipArchiveEntry = Dust.access(MIND_TAG_CONTEXT_TARGET, MIND_TAG_ACCESS_PEEK, null, RESOURCE_ASP_STREAM);
+			
+			String fName = DustUtils.getPostfix(name, File.separator);
+			String id = DustUtils.cutPostfix(fName, ".");
+			id = id.split("-")[0];
+
+			String idHash = DustUtils.getHash2(id, File.separator);
+			
+			File fDataRoot = Dust.access(MIND_TAG_CONTEXT_SELF, MIND_TAG_ACCESS_PEEK, null, MISC_ATT_CONN_TARGET, MISC_ATT_VARIANT_VALUE);			
+			File fDir = new File(fDataRoot, idHash);
+			File fData = new File(fDir, fName);
+			
+			if ( fDir.isDirectory() ) {
+				if ( fData.isFile() && (zipArchiveEntry.getSize() == fData.length())) {
+					return MIND_TAG_RESULT_READACCEPT;
+				}
+			} else {
+				fDir.mkdirs();
+			}
+			
+			ZipFile zipFile = Dust.access(MIND_TAG_CONTEXT_TARGET, MIND_TAG_ACCESS_PEEK, null, MISC_ATT_VARIANT_VALUE);
+
+			DustUtilsApache.unzipEntry(zipFile, zipArchiveEntry, fData);
+			
 		}
 
 		return MIND_TAG_RESULT_READACCEPT;
--- dev/XbrlTracer/src/hu/sze/uni/xbrl/edgar/XbrlEdgarBoot.java
+++ dev/XbrlTracer/src/hu/sze/uni/xbrl/edgar/XbrlEdgarBoot.java
@@ -14,16 +14,24 @@ public class XbrlEdgarBoot implements XbrlEdgarConsts {
 	}
 		
 	public static void zipRead() throws Exception {
+		MindHandle hZipFile = DustDevUtils.registerHandle("0", RESOURCE_ASP_URL);
+		Dust.access(hZipFile, MIND_TAG_ACCESS_SET, "work/xbrl/data/sources/edgar/submissions_00.zip", RESOURCE_ATT_URL_PATH);
+		
+		MindHandle hTargetDir = DustDevUtils.registerHandle("0", RESOURCE_ASP_URL);
+		Dust.access(hTargetDir, MIND_TAG_ACCESS_SET, "work/xbrl/data/sources/edgar/submissions", RESOURCE_ATT_URL_PATH);
 				
 		MindHandle hLogZipReader = DustDevUtils.registerLogic("0", DustZipAgentReader.class.getCanonicalName());
 		MindHandle hAgtZipReader = DustDevUtils.registerAgent("0", hLogZipReader);
 		
 		MindHandle hZipEntry = DustDevUtils.registerHandle("0", RESOURCE_ASP_STREAM);
 		
+		Dust.access(hAgtZipReader, MIND_TAG_ACCESS_SET, hZipFile, MISC_ATT_CONN_SOURCE);
 		Dust.access(hAgtZipReader, MIND_TAG_ACCESS_SET, hZipEntry, MISC_ATT_CONN_TARGET);
 		
 		MindHandle hLogUnzip = DustDevUtils.registerLogic("0", XbrlEdgarAgentUnzip.class.getCanonicalName());
 		MindHandle hAgtUnzip = DustDevUtils.registerAgent("0", hLogUnzip); 
+		
+		Dust.access(hAgtUnzip, MIND_TAG_ACCESS_SET, hTargetDir, MISC_ATT_CONN_TARGET);
 
 		Dust.access(hZipEntry, MIND_TAG_ACCESS_INSERT, hAgtUnzip, MIND_ATT_KNOWLEDGE_LISTENERS, KEY_ADD);
 
