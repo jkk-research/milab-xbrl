2f79318d6d40555ae9d8b3277967c942b31db45b
Lorand Kedves <lorand.kedves@gmail.com>
2024-03-05 16:34:50 +0100

    XbrlTracer - JSONAPI compatible author name

--- dev/XbrlTracer/src/hu/sze/uni/xbrl/XbrlConsts.java
+++ dev/XbrlTracer/src/hu/sze/uni/xbrl/XbrlConsts.java
@@ -5,7 +5,7 @@ import hu.sze.milab.dust.DustHandles;
 import hu.sze.milab.dust.utils.DustUtilsConsts;
 
 public interface XbrlConsts extends DustHandles, DustUtilsConsts {
-	String AUTHOR_XBRLDOCK = "XBRLDock.hu";
+	String AUTHOR_XBRLDOCK = "XBRLDock";
 	
 	MindHandle XBRLTEST_UNIT = Dust.lookup(AUTHOR_XBRLDOCK + DUST_SEP_ID + "XbrlTest");
 
